9982b4ceeb0d25fa9b6c534c5194d345803dfa97
Tobi Adegbuji <adegbuji_tobi@yahoo.com>
2021-04-05 21:13:17 -0400

    Added Bean Validation

--- src/main/java/dev/tobiadegbuji/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/dev/tobiadegbuji/msscbeerservice/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -15,20 +19,30 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
 
+    @Null //Req will fail validation if they try to pass this field (Defensive Programming)
     private UUID id;
 
+    @Null
     private Integer version;
 
+    @Null
     private OffsetDateTime createdDate; //Always use UTC when doing web apps
 
+    @Null
     private OffsetDateTime lastModifiedDate;
 
+    @NotBlank
     private String beerName;
 
+    @NotNull
     private BeerStyle beerStyle;
 
+    @NotNull
+    @Positive
     private Long upc;
 
+    @NotNull
+    @Positive
     private BigDecimal price;
 
     private Integer quantityOnHand;
