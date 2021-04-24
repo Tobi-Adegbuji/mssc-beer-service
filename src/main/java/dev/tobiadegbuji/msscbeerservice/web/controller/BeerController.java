27f657aca704e172f6ccdfa4fbd984635e277ac6
Tobi Adegbuji <adegbuji_tobi@yahoo.com>
2021-04-23 21:59:45 -0400

    Controller Impl

--- src/main/java/dev/tobiadegbuji/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/dev/tobiadegbuji/msscbeerservice/web/controller/BeerController.java
@@ -1,6 +1,8 @@
 package dev.tobiadegbuji.msscbeerservice.web.controller;
 
+import dev.tobiadegbuji.msscbeerservice.services.BeerService;
 import dev.tobiadegbuji.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,26 +12,24 @@ import java.util.UUID;
 
 @RequestMapping("/api/v1/beer")
 @RestController
+@RequiredArgsConstructor
 public class BeerController {
 
-    //private final BeerService beerService;
+    private final BeerService beerService;
 
     @GetMapping("/{beerId}")
-    public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID beerId){
-        //TODO: IMPL
-        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID beerId) {
+        return new ResponseEntity<>(beerService.getById(beerId), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity saveBeer(@Valid @RequestBody BeerDto beerDto){
-        //TODO: IMPL
-        return new ResponseEntity(HttpStatus.CREATED);
+    public ResponseEntity<BeerDto> saveBeer(@Valid @RequestBody BeerDto beerDto) {
+        return new ResponseEntity<>(beerService.saveNewBeer(beerDto),HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable UUID beerId){
-        //TODO: IMPL
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    public ResponseEntity<BeerDto> updateBeerById(@PathVariable UUID beerId, @Valid @RequestBody BeerDto beerDto) {
+        return new ResponseEntity<>(beerService.updateBeer(beerId,beerDto), HttpStatus.NO_CONTENT);
     }
 
 
